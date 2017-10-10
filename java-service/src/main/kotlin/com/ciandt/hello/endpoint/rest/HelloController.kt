ac4b84f9b10f94ad201e59bd4e249f7a84e97b3d
Sidharta Noleto <sidvelloso@gmail.com>
2017-10-10 17:28:01 -0300

    including dto into the project

--- java-service/src/main/kotlin/com/ciandt/hello/endpoint/rest/HelloController.kt
+++ java-service/src/main/kotlin/com/ciandt/hello/endpoint/rest/HelloController.kt
@@ -1,23 +1,27 @@
 package com.ciandt.hello.endpoint.rest
 
-import com.ciandt.hello.core.entity.HelloMessage
 import com.ciandt.hello.core.usecase.SayHelloUseCase
-import org.springframework.web.bind.annotation.*
-import io.swagger.annotations.*
+import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiResponse
+import io.swagger.annotations.ApiResponses
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("hello")
 class HelloController() {
 
     @Autowired
-    val sayHelloUC : SayHelloUseCase? = null
+    val sayHelloUC: SayHelloUseCase? = null
 
     @GetMapping
     @ApiOperation(value = "sayHello", nickname = "hello")
-	@ApiResponses(ApiResponse(code = 200, message = "Success", response =  HelloMessage::class ))
-    fun sayHello( @RequestParam(value="name", defaultValue="World") name: String ): HelloMessage {
+    @ApiResponses(ApiResponse(code = 200, message = "Success", response = HelloDto::class))
+    fun sayHello(@RequestParam(value = "name", defaultValue = "World") name: String): HelloDto {
         val msg = sayHelloUC!!.sayHello(name)
-        return msg
+        return HelloDto(message = msg.message)
     }
 }
---
+++ java-service/src/main/kotlin/com/ciandt/hello/endpoint/rest/HelloDto.kt
@@ -0,0 +1,6 @@
+package com.ciandt.hello.endpoint.rest;
+
+/**
+ * Created by sidharta on 10/9/17.
+ */
+data class HelloDto(val message: String = "")
